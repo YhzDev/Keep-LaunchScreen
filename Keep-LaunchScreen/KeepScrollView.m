7b31d8e6a78c51654e5841b2cd44e9586c1f412a
yu137988395 <137988395@qq.com>
2016-08-25 18:36:42 +0800

    08.25—commit

--- Keep-LaunchScreen/KeepScrollView.m
+++ Keep-LaunchScreen/KeepScrollView.m
@@ -80,6 +80,8 @@
     
     [self setContentOffset:CGPointMake(W, 0)];
     
+    //添加定时器
+    [self addTimer];
 }
 
 #pragma mark  - scrollviewDelegate
@@ -90,6 +92,75 @@
     int page = (scrollView.contentOffset.x + scrollW * 0.5) / scrollW;
     page --;
     self.pageControl.currentPage = page;
+}
+
+//开始拖拽视图
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self removeTimer];
+}
+
+//完成拖拽
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    [self addTimer];
+}
+
+//滚动动画停止时执行,代码改变时出发,也就是setContentOffset改变时
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView{
+    [self scrollViewDidStop:scrollView];
+}
+
+//减速停止了时执行，手触摸时执行执行
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    [self scrollViewDidStop:scrollView];
+}
+
+
+- (void)scrollViewDidStop:(UIScrollView *)scrollView
+{
+    CGFloat scrollW = [UIScreen mainScreen].bounds.size.width;
+    int page = (self.contentOffset.x + 0.5 * scrollW) / scrollW;
+    if (page == 0){
+        [self setContentOffset:CGPointMake(scrollW * self.titles.count, 0)];
+    } else if (page == (self.titles.count + 1)) {
+        [self setContentOffset:CGPointMake(scrollW , 0)];
+    }
+}
+
+- (void)nextImage
+{
+    NSInteger page = 0;
+    if (self.pageControl.currentPage == self.titles.count) {
+        page = 0;
+    } else {
+        page = self.pageControl.currentPage + 1;
+    }
+    CGFloat offsetX = (page + 1) * self.bounds.size.width;
+    CGPoint offset = CGPointMake(offsetX, 0);
+    
+    [self setContentOffset:offset animated:YES];
+    
+}
+
+- (void)addTimer
+{
+    //Timer会被加入到当前线程的Run Loop中，且模式是默认的NSDefaultRunLoopMode。而如果当前线程就是主线程，比如UIScrollView的拖动操作，会将Run Loop切换成NSEventTrackingRunLoopMode模式，在这个过程中，默认的NSDefaultRunLoopMode模式中注册的事件是不会被执行的。也就是说，此时使用scheduledTimerWithTimeInterval添加到Run Loop中的Timer就不会执行。
+    
+    self.timer = [NSTimer timerWithTimeInterval:3.0f target:self selector:@selector(nextImage) userInfo:nil repeats:YES];
+    
+    [[NSRunLoop currentRunLoop]addTimer:self.timer forMode:NSRunLoopCommonModes];
+}
+
+- (void)removeTimer
+{
+    [self.timer invalidate];
+    self.timer = nil;
+}
+
+- (void)dealloc
+{
+    [self removeTimer];
 }
 
 
